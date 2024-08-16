7aa7bb7971185094a3d5fe9fcc8d7a77522b9183
Worstik11 <sadistik22@gmail.com>
2024-08-16 17:27:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,9 @@ const { Classic } = require("musicard");
 const { prefix } = require('./config.json');
 const nodes = [
     {
-        host: "37.114.42.191",
-        port: 9906, 
-        password: "danteisnttaken", 
+        host: "lavalink.jompo.cloud",
+        port: 2333, 
+        password: "jompo", 
          secure: false
       },
 ];
