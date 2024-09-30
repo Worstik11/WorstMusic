fa59345cadc7fba88ed277d070499fd7f5c858ff
Worstik11 <sadistik22@gmail.com>
2024-09-30 13:54:46 +0300

    Update owner.js

--- utilityCommands/owner.js
+++ utilityCommands/owner.js
@@ -4,16 +4,15 @@ module.exports = {
     name: 'owner',
     description: 'Bot owner info',
     execute(message, args) {
-        const steamLink = 'https://steamcommunity.com/id/76561198379550457edit/';
+        const steamLink = 'https://steamcommunity.com/id/76561198379550457/';
         const discordLink = 'https://discord.gg/SEV5JvcZ';
+        
         const embed = new EmbedBuilder()
             .setColor('#FFFFFF')
-            .setTitle(' 🫅 Owner Info')
-            .setDescription(`__**About me**__:\n 🤖;
+            .setTitle('🫅 Owner Info')
+            .setDescription(`__**About me**__:\n 🤖 Owner of this bot!\n[Steam Profile](${steamLink})\n[Join Discord](${discordLink})`)
             .setTimestamp();
-
 
         message.reply({ embeds: [embed] });
     },
 };
-
