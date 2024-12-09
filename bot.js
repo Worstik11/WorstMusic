347e82673b1bcf40a2513b0602074e95b9d360b9
Worstik11 <sadistik22@gmail.com>
2024-12-09 22:36:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,9 @@ const { Classic } = require("musicard");
 const { prefix } = require('./config.json');
 const nodes = [
     {
-        host: "lavalink.jirayu.net",
-        port: 13592, 
-        password: "youshallnotpass", 
+        host: "lava-v4.ajieblogs.eu.org",
+        port: 80, 
+        password: "https://dsc.gg/ajidevserver", 
          secure: false
       },
 ];
