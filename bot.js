54424f40ecafabbcf0dc01fa10cdb2aad3ecca02
Worstik11 <sadistik22@gmail.com>
2025-08-15 02:39:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,9 @@ const { Classic } = require("musicard");
 const { prefix } = require('./config.json');
 const nodes = [
       {
-     "host": "lava-v4.ajieblogs.eu.org",
-    "port": 80,
-    "password": "https://dsc.gg/ajidevserver",
+     "host": "lavahatry4.techbyte.host",
+    "port": 3000,
+    "password": "NAIGLAVA-dash.techbyte.host",
     "secure": false,
       },
 ];
@@ -317,6 +317,7 @@ client.riffy.on("queueEnd", async (player) => {
 client.on("raw", (d) => {
     client.riffy.updateVoiceState(d);
 });
+
 
 
 
