dd867a6f51101381149684d10069d18c6522b627
Worstik11 <sadistik22@gmail.com>
2025-08-18 23:35:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,9 @@ const { Classic } = require("musicard");
 const { prefix } = require('./config.json');
 const nodes = [
       {
-     "host": "lavalink.devxcode.in",
-    "port": 443,
-    "password": "DevamOP",
+     "host": "lavalink.jirayu.net",
+    "port": 13592,
+    "password": "youshallnotpass",
     "secure": false,
       },
 ];
@@ -317,6 +317,7 @@ client.riffy.on("queueEnd", async (player) => {
 client.on("raw", (d) => {
     client.riffy.updateVoiceState(d);
 });
+
 
 
 
